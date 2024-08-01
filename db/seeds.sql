7fe2358954ba1467673ece96be59578dd89c1879
Jacob <jacobmrobison472@gmail.com>
2024-08-01 19:37:49 -0400

    more sql changes

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,19 +3,17 @@
 -- Insert sample data into the employees table
 INSERT INTO employees (id, name, age, department)
 VALUES (1, 'John Doe', 30, 'Sales'),
-                (2, 'Jane Doe', 25, 'Sales'),
-                (3, 'Alice Johnson', 35, 'Sales'),
+                (2, 'Pam Halpurt', 25, 'Sales'),
+                (3, 'Angela Martin', 3, 'Sales'),
                 (4, 'Bob Johnson', 40, 'Front Desk'),
                 (5, 'Jim Halpurt', 45, 'Assistant Manager'),
-                (6, 'Dwight Schrute', 35, 'Assistant to the Assistant Manager');
                 (7, 'Dwight Schrute', 35 , 'Manager');
 
 
 
 INSERT INTO departments (id, name, location)
 VALUES (1, 'assistant manager', 'scranton'),
-             (2, 'assistant to the asisstant manager', 'scranton'),
-             (3, 'sales', 'scranton'),
-             (4, 'front_desk', 'scranton');
-             (5, 'manager', 'scranton');
+             (2, 'sales', 'scranton'),
+             (3, 'front_desk', 'scranton');
+             (4, 'manager', 'scranton');
 
